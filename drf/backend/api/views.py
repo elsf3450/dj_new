00fe8180dc28ed1adae0308d542229f3ca876330
bill <elsf3458@gmail.com>
2022-06-15 13:44:26 +0800

    00

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,23 +1,15 @@
 
-from django.http import JsonResponse
+from pyexpat import model
+from django.http import JsonResponse,HttpResponse
+from products.models import Product
 # Create your views here.
 import json
 def api_home(request,*args,**kwargs):
-    body = request.body
-    data=json.loads(body)
-    print(data.keys())
-    print(data.values())
-    print(dict(request.headers))
-    print(request.content_type)
-    ''' header
-    {'Content-Length': '15',
-     'Content-Type': 'application/json',
-      'Host': '127.0.0.1:8000', 
-      'User-Agent':
-       'python-requests/2.18.4',
-        'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
-    '''
-    #data["headers"]=request.headers
-    #data["content_type"]=request.content_type
-    #print(body.decode('UTF-8','strict'))
-    return JsonResponse({"message":"HI there"})
+    model_data=Product.objects.all().order_by("?").first()
+    data={}
+    data['id']=model_data.id
+    data['title']=model_data.title
+    data['content']=model_data.content
+
+    data_r = json.dumps(data)
+    return HttpResponse(data_r,headers={"content_type":'application/json'})
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -2,4 +2,6 @@
 import requests
 endpoint="http://127.0.0.1:8000/api/"
 aa=requests.get(endpoint,params={"abc":123},json={"query":"HI"})
-print(aa.json())
+print(aa.text)
+print(aa.headers)
+print(aa.status_code)
